e1b2a012adf8014b5240765838ff0716e7e62a63
burryxie <kqx0731@163.com>
2023-09-19 05:48:08 +0000

    longze init

--- app.py
+++ app.py
@@ -1,43 +1,14 @@
-# import os
-
-# for file in os.listdir():
-#   if file.startswith('.'):
-#     print(file)
-
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-JOBS = [{
-    'ID': 1,
-    'Title': 'Data Engineering',
-    'Location': 'Shenzhen,China',
-    'Salary': 'RMB 20000'
-}, {
-    'ID': 2,
-    'Title': 'Data Architect',
-    'Location': 'Shenzhen,China',
-    'Salary': 'RMB 30000'
-}, {
-    'ID': 3,
-    'Title': 'Data Scientist',
-    'Location': 'Hongkong,China',
-    'Salary': 'RMB 40000'
-}]
+
+@app.route("/")
+def home():
+  # return "Hello, Flask!"
+  return render_template('index.html')
 
 
-@app.route('/')
-def home_page():
-  # return "Hello World"
-  return render_template("home.html"
-                         , jobs=JOBS
-                         , company_name="Hello")
-
-@app.route('/api/jobs')
-def get_all_jobs():
-  return jsonify(JOBS)
-
-
-
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+@app.route("/buss_info")
+def buss_info():
+  return render_template('buss_info.html')
